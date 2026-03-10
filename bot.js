69a85f85a9fe3bd776082bc8d8db36acefa97cf1
Jamron10 <narekmxeyan2025@gmail.com>
2026-03-10 22:34:43 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+app.use(express.static(__dirname));
+
 const PORT = process.env.PORT || 3000;
 
 // ==========================================
